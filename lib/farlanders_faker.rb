e0228481187e3d048963332d657bb6ddfa3a545f
Daniel Lv <lgn21st@gmail.com>
2009-04-29 22:43:25 +0800

    [UPDATE] can generate fake name for english and chinese

--- lib/farlanders_faker.rb
+++ lib/farlanders_faker.rb
@@ -1,6 +1,120 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'extensions/array'
+
 module FarlandersFaker
   VERSION = '0.0.1'
+  
+  @@default_locale = :en
+  @@name_faker = nil
+  
+  class << self
+    # Return the current default locale. Defaults to :en
+    def default_locale
+      @@default_locale
+    end
+    
+    # Return the current locale
+    def locale
+      defined?(I18n) ? I18n.locale : default_locale
+    end
+    
+    # Sets the current locale
+    def locale=(locale)
+      @@default_locale = locale
+    end
+    
+    # Return the current name faker
+    def name_faker
+      @@name_faker ||= FarlandersFaker::Name.new
+    end
+    
+    # Generate a faked name with current local
+    def name
+      name_faker.name(locale)
+    end
+  end
+  
+  class Name
+    
+    def names
+      @names ||= {}
+    end
+    
+    def init_names
+      Dir[File.dirname(__FILE__) + "/locale_names/*.yml"].each do |filename|
+        load_yml(filename)
+      end
+      @initialized = true
+    end
+    
+    # Loads a YAML names file. the data must have locales as
+    # toplevel keys.
+    def load_yml(filename)
+      require 'yaml' unless defined? YAML
+      data = YAML::load(IO.read(filename))
+      data.each {|locale, d| merge_names(locale, d)}
+    end
+    
+    def init_names?
+      @initialized ||= false
+    end
+    
+    # Deep merges the given name hash with the existing names
+    # for the given locale
+    def merge_names(locale, data)
+      locale = locale.to_sym
+      names[locale] ||= {}
+      data = deep_symbolize_keys(data)
+      
+      # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
+      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+      names[locale].merge!(data, &merger)
+    end
+    
+    # Returns an array of locales for which names are available
+    def available_locales
+      init_names unless init_names?
+      names.keys
+    end
+    
+    def default_locale
+      init_names unless init_names?
+      FarlandersFaker.default_locale
+    end
+    
+    def name(locale)
+      init_names unless init_names?
+      locale = default_locale unless available_locales.include?(locale)
+      
+      format.rand.map do |key|
+        names[locale][key]
+      end.join(' ').strip
+    end
+    
+    def format
+      [
+        [:prefix, :first_name, :last_name],
+        [:first_name, :last_name, :suffix],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name],
+        [:first_name, :last_name]
+      ]
+    end
+    
+    # Return a new hash with all keys and nested keys converted to symbols.
+    def deep_symbolize_keys(hash)
+      hash.inject({}) { |result, (key, value)|
+        value = deep_symbolize_keys(value) if value.is_a? Hash
+        result[(key.to_sym rescue key) || key] = value
+        result
+      }
+    end
+  end
 end
--- spec/farlanders_faker_spec.rb
+++ spec/farlanders_faker_spec.rb
@@ -2,10 +2,104 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 # Time to add your specs!
 # http://rspec.info/
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+describe FarlandersFaker do
+  before(:each) do
+    Object.instance_eval do
+      module I18n
+        class << self
+          def locale
+            :en
+          end
+        end
+      end
+    end
   end
   
+  describe "locale without rails" do
+    before(:each) do
+      Object.send(:remove_const, :I18n) if defined?(I18n)
+    end
+    
+    it "should have default locale" do
+      FarlandersFaker.locale.should == :en
+    end
+    
+    it "can sets the current locale" do
+      FarlandersFaker.locale = :new_locale
+      FarlandersFaker.locale.should == :new_locale
+      
+    end
+  end
+  
+  describe "locale with rails I18n support" do
+    after(:each) do
+      FarlandersFaker.locale = :en
+    end
+    
+    it "should reading locale from rails I18n support" do
+      I18n.should_receive(:locale).and_return :locale_from_rails_I18n
+      FarlandersFaker.locale.should == :locale_from_rails_I18n
+    end
+    
+    it "do not override rails' locale setting" do
+      I18n.should_receive(:locale).and_return :locale_from_rails_I18n
+      FarlandersFaker.locale = :reset_locale
+      FarlandersFaker.locale.should == :locale_from_rails_I18n
+    end
+  end
+  
+  describe "generate names by name faker" do
+    it "should delegate name to name fakder with current locale" do
+      FarlandersFaker.should_receive(:locale).and_return(:current_locale)
+      FarlandersFaker.name_faker.should_receive(:name).with(:current_locale)
+      FarlandersFaker.name
+    end
+  end
+  
+  describe "name faker" do
+    it "should respond to name_faker" do
+      FarlandersFaker.should be_respond_to(:name_faker)
+    end
+    
+    it "should instantiated FarlandersFaker::Name" do
+      FarlandersFaker.name_faker.should be_is_a(FarlandersFaker::Name)
+    end
+    
+    it "can store names template" do
+      faker = FarlandersFaker::Name.new
+      name_sets = {:first_name => %w(Daniel), :last_name => %w(Lv)}
+      faker.merge_names :en, name_sets
+      faker.names.should == {:en => name_sets}
+    end
+    
+    describe FarlandersFaker::Name do
+      before(:each) do
+        @faker = FarlandersFaker::Name.new
+        @faker.stub!(:init_names?).and_return(true)
+        @faker.merge_names :en, {:first_name => %w(Daniel), :last_name => %w(Lv)}
+      end
+      
+      it "should have available locales" do
+        @faker.available_locales.should == [:en]
+      end
+      
+      it "should have default locales" do
+        FarlandersFaker.should_receive(:default_locale).and_return(:en)
+        @faker.default_locale.should == :en
+      end
+      
+      it "should generate name with default locale" do
+        @faker.name(:en).should == "Daniel Lv"
+      end
+      
+      it "will using default locale if special locale was not exists" do
+        @faker.name(:zh).should == "Daniel Lv"
+      end
+      
+      it "can add more locale support" do
+        @faker.merge_names :zh, {:first_name => %(Rick), :last_name => 'Hu'}
+        @faker.name(:zh).should == "Rick Hu"
+      end
+    end
+  end
 end
